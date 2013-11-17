efd29b6d28b72ebdb82e52b868d79b1e36ff69db
David Holowiski <david@holowiski.com>
2013-11-17 16:15:39 -0700

    added a bunch of error checking changed so user must create config.php

--- import.php
+++ import.php
@@ -3,54 +3,104 @@
 
 require 'meekrodb/db.class.php';
 
-/* Config */
+//try to open the config file
+if (!(include 'config.php')) {
+	echo("ERROR: Couldn't open config.php\n");
+	echo("In order to run this script you must copy config.php.sample to config.php, then edit the file and supply the appropriate information\n");
+	exit(1); //error, couldn't open config.php
+}
 
-DB::$user = 'root';
-DB::$password = 'root';
-DB::$dbName = 'thinkup';
-DB::$host = 'localhost';
+if ($verbose && $dry_run) {
+	echo("*************************\n");
+	echo("Script is configed for a dry run. This will preview what will happen, but the database will not be modified.\n");
+	echo("To run for real, change \$dry_run to true in config.php\n");
+	echo("*************************\n");
+}
 
-// Change this if you changed your posts table prefix
-$table_name = 'tu_posts';
-
-// Twitter user ID (number, can be found in the tweet archive)
-$userid = '';
-
-$directory = "tweets/";
-
-$timezone = 'America/New_York';
-
-/* Script Start */
-
+//set up initial variables
+if (!$userid) { $userid = getTwitterUseridNumber($directory); } //if userid isn't in config.php, attempt to detect it
+$tweet_files = glob($directory.$directory_js_location);
+$tweet_files_count = count($tweet_files);
 date_default_timezone_set($timezone);
 
-$tweet_files = glob($directory."data/js/tweets/*.js");
+//informational counts
+$tweets_in_files = 0;
+$tweets_added = 0;
+$tweets_duplicate = 0;
+$tweet_files_processed = 0;
 
+if (!checkToProceed($userid, $tweet_files, $timezone, $verbose)) {exit(2);} 
+
+//start the import
 $existing = getExistingTweets($table_name, $userid);
-	
+if ($verbose) { echo("There are ".count($existing)." existing tweets for userid ".$userid.", located in ".count($tweet_files)." archive files\n"); }
 foreach($tweet_files as $file)
 {
 	$parsedFile = parseFile($file);
 	$parsedTweets = parseTweets($parsedFile);
-
+	if ($show_progress) {echo("Processing file ".$tweet_files_processed." of ".$tweet_files_count." - tweets:".count($parsedTweets)."\n");}
 	foreach($parsedTweets as $tweet){
-		insertTweet($tweet, $table_name, $existing);
+		insertTweet($tweet, $table_name, $existing, $dry_run);
 	}
+	$tweet_files_processed++;
+}
+//finished the import
 
+if ($verbose) { echo($tweets_added." Tweets were added to thinkup, and ".$tweets_duplicate." tweets were ignored because they already exist\n");}
+if ($verbose && $dry_run) { echo("If you liked what you saw, change \$dry_run to true and run this again.\n"); }
+
+exit(0); //completed successfully
+
+//functions below
+function checkToProceed($userid, $tweet_files, $timezone, $verbose) {
+	if ($userid == '') {
+		if ($verbose) {echo("ERROR: Twitter userid number was not supplied, and could not be detected. Please see the readme.\n"); }
+		return false;
+	}
+	if (!$tweet_files) {
+		if ($verbose) {echo("ERROR: Unable to find your tweet archive. \n"); }
+		return false;
+	}
+	if (!$timezone || $timezone=='') {
+		if ($verbose) {echo("ERROR: Time zone not set. \n"); }
+		return false;
+	}
+	return true;
+}
+
+function getTwitterUseridNumber($directory) {
+	$userid_location = 'data/js/user_details.js';
+	$file_contents = file_get_contents($directory.$userid_location);
+	if ($file_contents) {
+		$id_string_loc = strpos($file_contents, "\"id\" : \"");
+		if ($id_string_loc) {
+			$id_string_loc_start = $id_string_loc + 8;
+			$id_string = substr($file_contents, $id_string_loc_start);
+			$id_end_loc = strpos($id_string, "\",");
+			$final_id = substr($id_string, 0, $id_end_loc);
+			$twitter_userid_number = trim($final_id);
+			if (twitter_userid_number) {
+				return $twitter_userid_number;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}	
+	} else {
+		return false;
+	}
 }
 
 function parseFile($file){
-
 	$file_contents = file_get_contents($file);
 	$str_data = substr($file_contents, 32);
-	
 	$data = json_decode($str_data);
-
 	return $data;
-
 }
 
 function parseTweets($tweets){
+	global $tweets_in_files;
 
 	$parsed_tweets = array();
 
@@ -84,14 +134,18 @@ function parseTweets($tweets){
 
 		array_push($parsed_tweets, $parsed_tweet);
 	}
-
+	$tweets_in_files = $tweets_in_files + count($parsed_tweets);
 	return $parsed_tweets;
 
 }
 
-function insertTweet($tweet, $table_name, $existing){
+function insertTweet($tweet, $table_name, $existing, $dry_run){
+	global $tweets_added, $tweets_duplicate;
 	if (!isset($existing[$tweet['post_id']])) {
-		DB::insert($table_name, $tweet);
+		if (!$dry_run){DB::insert($table_name, $tweet);}
+		$tweets_added++;
+	} else {
+		$tweets_duplicate++;
 	}
 }
 
